4838cc788c6d7eaca48a441d5abda485690369be
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-04-12 16:23:51 -0500

    Cleaned the pages.

--- src/App.js
+++ src/App.js
@@ -10,23 +10,6 @@ function App() {
         <WeatherSearch />
         <CityDisplay icon="CLEAR_DAY" size={200} />
       </div>
-        
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      
     </div>
     
   );
